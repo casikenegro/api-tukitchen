c7245a56dd24f4b95447db8f8eed132af9f8d4c6
casikenegro <ortizjesusrivas@gmail.com>
2021-02-12 23:57:51 -0400

    wip

---
+++ seeders/20210213035127-product_categories.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('product_categories', 
+    [
+      {
+        product_id: 1,// sopa de macaco
+        category_id: 1, //carne
+      }, 
+      {
+        product_id: 1,// sopa de macaco
+        category_id: 2, //sopa
+      },
+      {
+        product_id: 2,//carne con papas
+        category_id: 1, //carne
+      },
+      {
+        product_id: 2,//carne con papas
+        category_id: 3, //papas
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('product_categories', null, {});
+  }
+};
