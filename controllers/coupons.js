164cf8fa17e816fb7ce6e1d8b490e467c6325cb8
Cesar <cesarmiguel199@gmail.com>
2021-04-01 17:52:42 -0400

    bug

--- controllers/coupons.js
+++ controllers/coupons.js
@@ -66,6 +66,7 @@ async function createByAdmin(req,res){
     if(!profile) return res.status(403).send({message:"profile not create"});
     const coupon = await models.Coupons.create({
       discount:req.body.discount /100,
+      name:req.body.name,
       profile_id: profile.id, 
       is_used:false,
     });
